955a3a1bb34b1e68ee070a591a4e6f47d3498adc
motebhushan <motebhushan6524@gmail.com>
2025-07-22 17:12:54 +0530

    changing the MOngo url and frontend url for cors

--- backend/src/lib/db.js
+++ backend/src/lib/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017/chat-app");
+    const conn = await mongoose.connect("mongodb+srv://motebhushan6524:6QZhAzUnghkqqqTD@cluster0.kepsvut.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0");
     console.log(`MongoDB connected: ${conn.connection.host}`);
   } catch (error) {
     console.log("MongoDB connection error:", error);
